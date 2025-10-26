d1add3a19ce0451c33273ccf091a7bfe0063abac
Esther Chen <yuepichu@gmail.com>
2025-10-25 21:39:39 -0400

    made homepage pretty somewhat

--- game-container/src/App.jsx
+++ game-container/src/App.jsx
@@ -9,17 +9,19 @@ function App(){
 
   return (
     <BrowserRouter>
-      <nav>
-          <Link to="/">[Home Page] </Link>
-          <Link to="/GamesCollection">[Games Collection] </Link>
-          <Link to="/Login">[Login Page] </Link>
-      </nav>
+      
 
       <Routes>
         <Route path="/" element={<TempMainPage/>} />
         <Route path="/GamesCollection" element={<GameCollectionPage/>} />
         <Route path="/Login" element={<LoginPage/>} />
       </Routes>
+
+      <nav>
+          <Link to="/">[Home Page] </Link>
+          <Link to="/GamesCollection">[Games Collection] </Link>
+          <Link to="/Login">[Login Page] </Link>
+      </nav>
     </BrowserRouter>
   );
 }
